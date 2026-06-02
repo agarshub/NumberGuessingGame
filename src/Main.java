40a52f8b89b466f700ad36d972b66d62c6cd2e42
agarshub <shubhamagarwal0111@gmail.com>
2026-06-02 17:02:18 +0530

    Version 0.3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,67 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Random;
 
+class NumberGuesser
+{
+    public int getValidInt(Scanner sc)
+    {
+        int input=0;
+        while(true)
+        {
+            System.out.print("Guess a number: ");
+            try
+            {
+            input = sc.nextInt();
+            if(input<1 || input>100)
+            {
+                throw new IllegalArgumentException("Number must be between 1 and 100!");
+            }
+            break;
+            }
+            catch(InputMismatchException e)
+            {
+            System.out.println("Please enter a valid number: "+e);
+            sc.nextLine();
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println(e);
+            }
+        }
+        return input;
+    }
+
+    public void performanceMeasure(int attempts)
+    {
+        switch(attempts)
+        {
+        case 1,2,3 : System.out.println("Excellent!!");
+            break;
+        case 4,5,6,7 : System.out.println("Good!");
+            break;
+        default: System.out.println("Keep Practicing..");
+        }
+    }
+
+}
 public class Main
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Guess a number between 1 and 100");
         Random r = new Random();
+        NumberGuesser ng = new NumberGuesser();
+
         int randomNumber = r.nextInt(100) + 1;
 
+        System.out.println("Guess a number between 1 and 100"); 
+        int attempts = 0;
         int guessedNumber;
         while(true)
         {
-            System.out.print("Guess a number: ");
-            guessedNumber = sc.nextInt();
+            guessedNumber = ng.getValidInt(sc);
+            attempts++;
 
             if(guessedNumber > randomNumber)
             {
@@ -28,6 +74,8 @@ public class Main
             else
             {
                 System.out.println("Congrats! You guessed the right number");
+                System.out.println("Attempts: "+attempts);
+                ng.performanceMeasure(attempts);
                 sc.close();
                 break;
             }
@@ -35,7 +83,7 @@ public class Main
     }
 }
 
-/* Version 0.1
+/* Version 0.3
 
 Learnt and Implented:
 1. Constructors of classes are activated while creating the object of the class.
@@ -43,16 +91,23 @@ Learnt and Implented:
         Here new Scanner is a constructor.
         b. Random r = new Random();
         Here new Random() is a constructor.
+2. IllegalArgumentException is an exception used for runtime Exception,
+    which indicates that the method has been passed an illeagal argument,
+    according to the rules of the class/method.
 
 
 Other Learnings:
 1. r.nextInt(100) selects a random number from 0 to 99,
     where as r.nextInt(100)+1 selects a random number from 1 to 100.
-
+2. no need to attempts++ in every condition. If I take any guess I can write attempts++ once.
+3. Note that the attempts wont increment until the user enters a valid input.
+    So for every bad input the program will not increment the attempt counter,
+    hence resulting in robustness of the program.
+4. PerformanceMeasure method is an example of a behavior based on state.
 
 OOPS concept:
 1. new Random() creates a new random number generator. It is a constructor
-    nextInt() is used to select any random integer from negative 10 digit to positive 10 digit number
+    Since java int is 32 bit, nextInt() is used to select a number from -2,147,483,648 to 2,147,483,647
     nextInt(int) is used to select any number from 0 (inclusive) to int (parameter bound) randomly.
 
 */
